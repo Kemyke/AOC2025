2e31c975d2df9baf80725bc387c6d8af81744ea3
kemenya <attila_kemeny@trimble.com>
2025-12-02 10:01:36 +0100

    Day01 and Day02

---
+++ Day01/Program.cs
@@ -0,0 +1,43 @@
+﻿var input = File.ReadAllLines("input.txt");
+long ret1 = 0;
+long ret2 = 0;
+
+var pos = 50;
+foreach(var line in input)
+{
+    var d = line[0];
+    var a = int.Parse(line.Substring(1));
+    var z = pos == 0;
+
+    if(d=='L')
+    {
+        pos -= a;
+    }
+    else
+    {
+        pos += a;
+    }
+    if (pos <= 0)
+    {
+        if (pos % 100 == 0)
+            ret1++;
+        ret2 += (-1 * pos / 100) + ((z) ? 0 : 1);
+    }
+    if (pos >= 100)
+    {
+        if (pos % 100 == 0)
+            ret1++;
+        ret2 += pos / 100;
+
+    }
+    pos %= 100;
+    if (pos < 0)
+        pos += 100;
+}
+
+
+Console.WriteLine(ret1);
+Console.WriteLine(ret2);
+//2473 too low
+//6008 nem jó
+Console.ReadLine();
---
+++ Day02/Program.cs
@@ -0,0 +1,38 @@
+﻿var input = File.ReadAllText("input.txt");
+var l = input.Split(",").Select(x => x.Split("-"));
+
+long ret1 = 0;
+long ret2 = 0;
+
+foreach(var r in l)
+{
+    var s = long.Parse(r[0]);
+    var e = long.Parse(r[1]);
+
+    for (var i = s; i <= e; i++)
+    {
+        var istr = i.ToString();
+        if (istr.Length % 2 == 0)
+        {
+            if (istr.Substring(0, istr.Length / 2) == istr.Substring(istr.Length / 2))
+            {
+                ret1 += i;
+            }
+        }
+
+        for (var p = 1; p <= istr.Length / 2; p++)
+        {
+            var ptrn = istr.Substring(0, p);
+            if(istr.Replace(ptrn, "").Length==0)
+            {
+                ret2 += i;
+                break;
+            }
+
+        }
+    }
+}
+
+Console.WriteLine(ret1);
+Console.WriteLine(ret2);
+Console.ReadLine();
